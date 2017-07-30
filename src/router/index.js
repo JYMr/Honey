e9d77a6a67d9df088e169f8a1fdda730119fcfe6
JYMr <abbyhg@live.com>
2017-07-30 12:57:11 +0800

    Signed-off-by: JYMr <abbyhg@live.com>

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,68 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Index from '@/components/Index/Index/Index'
+import Cart from '@/components/Index/Cart/Cart'
+import Home from '@/components/Index/Home/Home'
+
+import Foot from '@/components/Common/Foot/Foot'
+
+import ProdustDetail from '@/components/Goods/ProdustDetail/ProdustDetail'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
+const routes = [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+      name: 'Index',
+      components: Index
+    },
+	{
+		path: '*',
+		components : Index,
+		meta: {
+			title: '404!'
+		}
+	}
+]
+
+const NavBottom = [
+	{
+		path: '/Index',
+		components:{
+			content:Index,
+			foot:Foot
+		}
+	},
+	{
+		path: '/Cart',
+		components:{
+			content: Cart,
+			foot: Foot
+		}
+	},
+	{
+		path: '/Home',
+		components:{
+			content: Home,
+			foot: Foot
+		}
+	}
+]
+
+const Goods = [
+	{
+		path: '/Goods/ProdustDetail',
+		components:{
+			content: ProdustDetail
+		}
+	}
+]
+
+const router = new Router({
+	mode: 'history',
+	routes: routes.concat(NavBottom,Goods)
 })
+
+router.push('Index')
+
+export default router
